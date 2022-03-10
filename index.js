ab901daff558cd6420e1762fc85c9d8d685965cd
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-03-10 16:28:26 -0600

    [Experiment] Add crappy example of interaction with python

--- index.js
+++ index.js
@@ -10,9 +10,45 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+        ls.stdin.write(`${msg}\n`);
     });
 });
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+const { spawn } = require('child_process');
+const ls = spawn('py', ['-u', 'x.py']); // -u -> unbuffered
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    io.emit('chat message', ab2str(data));
+});
+
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+    io.emit('chat message', data);
+});
+
+ls.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+    io.emit('chat message', data);
+});
+
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    io.emit('chat message', code);
+});
+
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
