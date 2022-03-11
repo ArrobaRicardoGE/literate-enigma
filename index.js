9f0ab78e27f06d52787a44bf99a13a2d7d006d93
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-03-11 13:01:07 -0600

    [Runner] Test Python runner

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const { PythonRunner } = require('./runner');
+
+const runner = new PythonRunner(io);
+runner.run('x.py');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -10,45 +14,10 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
-        ls.stdin.write(`${msg}\n`);
+        runner.write(msg);
     });
 });
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
-const { spawn } = require('child_process');
-const ls = spawn('py', ['-u', 'x.py']); // -u -> unbuffered
-
-ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-    io.emit('chat message', ab2str(data));
-});
-
-ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-    io.emit('chat message', data);
-});
-
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-    io.emit('chat message', data);
-});
-
-ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    io.emit('chat message', code);
-});
-
-function ab2str(buf) {
-    return String.fromCharCode.apply(null, new Uint16Array(buf));
-}
-function str2ab(str) {
-    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
-    for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-}
---
+++ runner.js
@@ -0,0 +1,37 @@
+const { spawn } = require('child_process');
+const { timingSafeEqual } = require('crypto');
+const { ab2str } = require('./util');
+
+class Runner {
+    constructor(io) {
+        this.io = io;
+    }
+    run(filename) {
+        let args = this.args;
+        args.push(filename);
+        this.process = spawn(this.command, args);
+        this.process.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+            this.io.emit('chat message', ab2str(data));
+        });
+        this.process.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+    }
+
+    write(data) {
+        this.process.stdin.write(`${data}\n`);
+    }
+}
+
+class PythonRunner extends Runner {
+    constructor(io) {
+        super(io);
+        this.command = 'py';
+        this.args = ['-u'];
+    }
+    static command = 'py';
+    static args = ['-u'];
+}
+
+module.exports = { PythonRunner };
