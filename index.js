31983238c3b6bd8021d36a2da9fa01dc4f6ecf10
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-03-11 13:34:56 -0600

    [Experiment] Wrong directory lol

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const fileUpload = require('express-fileupload');
 const { PythonRunner } = require('./runner');
+const port = process.env.PORT || 3000;
+
+app.use(fileUpload());
 
 const runner = new PythonRunner(io);
-runner.run('x.py');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/temp/' + sampleFile.name; // use join
+
+    // Use the mv() method to place the file somewhere on your server
+    console.log(uploadPath);
+    sampleFile.mv(uploadPath, function (err) {
+        if (err) return res.status(500).send(err);
+        runner.run(uploadPath);
+        res.redirect('/');
+    });
 });
 
 io.on('connection', (socket) => {
--- x.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-from time import sleep
-a = int(input())
-print("Hello")
-b = int(input())
-print("world")
-sleep(3)
-print(a + b)
